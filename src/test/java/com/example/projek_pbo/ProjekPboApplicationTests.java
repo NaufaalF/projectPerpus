d2236ea6c76b0f14ff230148f796cf49e7bcbcde
Raihan Daiva Danubrata <raihan.daiva@mhs.itenas.ac.id>
2025-05-27 12:54:51 +0700

    1.3.0
    login page, register page

---
+++ src/test/java/com/example/projek_pbo/ProjekPboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projek_pbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekPboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
